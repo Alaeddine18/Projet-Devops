02706817915553024c177ba4d25611fe79bee5a6
Alaeddine18 <adhuilaa@gmail.com>
2023-11-24 17:51:41 +0100

    fix workflow

--- scripting/deploy.sh
+++ scripting/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Tirer la nouvelle image Docker
+# Tirer la nouvelle image Docker (j'ai mis la platforme sinon sa ne marche pas comme je suis sur mac m1 ( stackoverflow ))
 docker pull ala91/priceapi:latest --platform linux/amd64
 
 # Arrêter et supprimer le conteneur existant s'il y en a un
