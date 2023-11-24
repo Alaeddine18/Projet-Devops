bb45932f5a95f28e20095411d6943a9d660307e3
Alaeddine18 <adhuilaa@gmail.com>
2023-11-24 17:47:27 +0100

    fix workflow

--- scripting/deploy.sh
+++ scripting/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Tirer la nouvelle image Docker
-docker pull ala91/priceapi:latest/x86_64
+docker pull ala91/priceapi:latest --platform linux/amd64
 
 # Arrêter et supprimer le conteneur existant s'il y en a un
 docker stop priceapi-container
